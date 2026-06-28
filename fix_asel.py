67d8f6bbc4adcc40a4a9168d68de179a7a37cf79
zelfcy <samirsahipkereev@gmail.com>
2026-06-28 07:50:09 +0500

    chore: Remove temp files, old CSS, staticfiles, pyc cache - major cleanup

--- fix_asel.py
+++
@@ -1,5 +0,0 @@
-from appointments.models import User, UserSubscription
-users = User.objects.filter(first_name__icontains='асель')
-for u in users:
-    UserSubscription.objects.filter(user=u).delete()
-    print(f"Removed subscriptions for {u.username}")
--- reset_revenue.py
+++
@@ -1,3 +0,0 @@
-from appointments.models import PaymentTransaction
-deleted_count, _ = PaymentTransaction.objects.all().delete()
-print(f"Deleted {deleted_count} payment transactions. Revenue is now 0.")
